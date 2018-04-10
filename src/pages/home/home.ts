990ccfe25702f9a7cf9705373ce817c7dee960af
glimbeek <gvanlimbeek@hotmail.com>
2018-04-10 15:51:57 +0200

    First commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+import { Firebase } from '@ionic-native/firebase';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController,
+              public firebase: Firebase) {
+
+      this.firebase.getToken()
+      .then(token => console.log(`The token is ${token}`)) // save the token server-side and use it to push notifications to this device
+      .catch(error => console.error('Error getting token', error));
+
+      this.firebase.onTokenRefresh()
+      .subscribe((token: string) => console.log(`Got a new token ${token}`));
+
+  }
+
+
+}
