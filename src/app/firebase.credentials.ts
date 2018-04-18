184847269df75a70f62039ae692ee8783760ee12
Limbeek van <LimbeevS@dz.local>
2018-04-18 08:12:56 +0200

    changed firebase project name

--- src/app/firebase.credentials.ts
+++ src/app/firebase.credentials.ts
@@ -1,8 +1,8 @@
 export const FIREBASE_CONFIG = {
-    apiKey: "AIzaSyCqK7OkLtVm_9iFY0uYvuUeS-03_YKgWEw",
-    authDomain: "KoersAlarm.firebaseapp.com",
-    databaseURL: "https://KoersAlarm.firebaseio.com",
-    projectId: "KoersAlarm",
-    storageBucket: "KoersAlarm.appspot.com",
-    messagingSenderId: "375761944772"
+    apiKey: "AIzaSyA_6MSyB2YwHrSo8G7T_mBFS9QV3_xuJtE",
+    authDomain: "koersalarm.firebaseapp.com",
+    databaseURL: "https://koersalarm.firebaseio.com",
+    projectId: "koersalarm",
+    storageBucket: "",
+    messagingSenderId: "540292576821"
 };
